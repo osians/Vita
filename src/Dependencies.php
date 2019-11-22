5b988c3bd16346e29b1e82443f02d9e4f43b8868
Wanderlei Santana <sans.pds@gmail.com>
2019-11-22 00:29:47 -0300

    Corrigindo erro instancia loader twig.

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -15,13 +15,34 @@ $injector->define('Http\HttpRequest', [
 $injector->alias('Http\Response', 'Http\HttpResponse');
 $injector->share('Http\HttpResponse');
 
-$injector->delegate('Twig_Environment', function () use ($injector) {
+-- --------------------------------------------------------------------------===
+    public function init_tpl_system($path)
+    {
+
+        \Twig_Autoloader::register();
+        $loader = new \Twig_Loader_Filesystem( $path );
+
+        # verificando se o modo de cache esta liberado no arquivo de config
+        $cacheFolder = ($this->config->twig_cache_enable === true)
+                     ? $this->config->system_path . 'cache' . DIRECTORY_SEPARATOR
+                     : false;
+
+        # instanciando o ambiente twig
+        $this->twig = new \Twig_Environment($loader,
+            array(
+                'cache' => $cacheFolder,
+                'debug' => $this->config->twig_debug_enable
+            )
+        );
+    }
+-- --------------------------------------------------------------------------===
+
+
+$injector->delegate('Twig_Environment', function () {
     $loader = new Twig_Loader_Filesystem(dirname(__DIR__) . '/templates');
     $twig = new Twig_Environment($loader);
     return $twig;
 });
-
-$injector->alias('Vita\Template\Renderer', 'Vita\Template\TwigRenderer');
 
 $injector->alias('Vita\Template\Renderer', 'Vita\Template\TwigRenderer');
 $injector->define('Vita\Page\FilePageReader', [
@@ -31,6 +52,7 @@ $injector->define('Vita\Page\FilePageReader', [
 $injector->alias('Vita\Template\FrontendRenderer', 'Vita\Template\FrontendTwigRenderer');
 $injector->alias('Vita\Page\PageReader', 'Vita\Page\FilePageReader');
 $injector->share('Vita\Page\FilePageReader');
+
 $injector->alias('Vita\Menu\MenuReader', 'Vita\Menu\ArrayMenuReader');
 $injector->share('Vita\Menu\ArrayMenuReader');
 
